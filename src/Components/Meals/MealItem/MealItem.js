96ca5572c50e563a2d12a7d088fdc6d9ee374623
Nishan Sarkar <76003942+Nishan202@users.noreply.github.com>
2022-07-25 19:54:31 +0530

    Update MealItem.js
    There is  a change of $ to indian rupee symbol

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,13 +1,13 @@
 import cssFile from "./MealItem.module.css";
 
 function MealItem(props) {
-  const price = `$${props.price.toFixed(2)}`;
+  //const price = `$${props.price.toFixed(2)}`;
   return (
     <li className={cssFile.meal}>
       <div>
         <h3 className={cssFile["meal h3"]}>{props.name}</h3>
         <div className={cssFile.description}>{props.description}</div>
-        <div className={cssFile.price}>{price}</div>
+        <div className={cssFile.price}><b>₹</b>{props.price}</div>
       </div>
       <div></div>
     </li>
